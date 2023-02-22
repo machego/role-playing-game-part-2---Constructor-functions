b29260a30736e12b8312ea79c78e393aa8dd9c12
machego <114348981+machego@users.noreply.github.com>
2023-02-22 19:57:47 +0100

    first version

---
+++ index.js
@@ -0,0 +1,24 @@
+import characterData from './data.js'
+import Character from './Character.js'
+
+
+const wizard = new Character( characterData.hero);
+const orc = new Character( characterData.monster);
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+}
+render();
+
+
+
+
+
+
+
+
+
+
+
+
